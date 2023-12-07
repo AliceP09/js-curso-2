d481fe1e6b593514594b659433f3e0ca9782a1c6
AliceP09 <alice.09pereira7@gmail.com>
2023-12-07 19:25:29 -0300

    Adicionando exercicio de fatoracao

--- app.js
+++ app.js
@@ -17,13 +17,20 @@ alert(`O resultado do calculo do IMC é ${resultadoIMC}`);
 alert('Bem-vindo ao calculo de fatorial');
 
 let numero = prompt('Digite um número de 1 a 20');
-let result = numero;
-let resultadoFatorial = 0;
+let result = 0;
+let contador = numero - 1;
+let iniciar = true;
 
-while (numero > 0){
-    result = result * (numero - 1);
-    resultadoFatorial = (resultadoFatorial + result);
-    console.log(`!${numero}`);
-    console.log(resultadoFatorial);
-    numero = numero - 1;
+while (contador > 0){
+    result = result == 0 ? numero * contador : result * contador;
+    mensagem();
+    contador = contador - 1;
+    iniciar = false;
 }
+
+function mensagem() {
+    return iniciar == true ? console.log(`Fatorial de !${numero}: ${numero} * ${contador} = ${result}`)
+        :    console.log(`Fatorial de !${numero}: ${result} * ${contador} = ${result}`);
+}
+
+console.log(`Ao final o fatorial de !${numero} é ${result}`);
